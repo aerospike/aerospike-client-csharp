c0f8cb84d00411a21491b87a1c91f47c6c69a212
Brian Nichols <bnichols@aerospike.com>
2015-11-06 17:22:21 -0800

    Update version 3.1.6

--- AerospikeClient/Properties/AssemblyInfo.cs
+++ AerospikeClient/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1")]
-[assembly: AssemblyFileVersion("3.1.5")]
+[assembly: AssemblyFileVersion("3.1.6")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AerospikeTest")]
