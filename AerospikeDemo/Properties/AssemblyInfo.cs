38bb7a6f5d226856073e860bad23e1a3e51c9a04
Brian Nichols <bnichols@aerospike.com>
2016-01-27 15:55:49 -0800

    Update copyright year.

--- AerospikeDemo/Properties/AssemblyInfo.cs
+++ AerospikeDemo/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aerospike")]
 [assembly: AssemblyProduct("AerospikeDemo")]
-[assembly: AssemblyCopyright("Copyright © 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © 2012-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
