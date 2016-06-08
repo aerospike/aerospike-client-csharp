e5875e12ccd6b0f6e705819bbca32cef5c779dcc
Brian Nichols <bnichols@aerospike.com>
2016-06-08 14:49:20 -0700

    Update version 3.2.3

--- AerospikeDemo/Properties/AssemblyInfo.cs
+++ AerospikeDemo/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.2")]
-[assembly: AssemblyFileVersion("3.2.2")]
+[assembly: AssemblyFileVersion("3.2.3")]
