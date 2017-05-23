575ba9f2be13c98b089d6063a69eabfadde6e7be
Brian Nichols <bnichols@aerospike.com>
2017-05-23 14:26:45 -0700

    Update version 3.4.3

--- Framework/AerospikeTest/Properties/AssemblyInfo.cs
+++ Framework/AerospikeTest/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("3.4")]
-[assembly: AssemblyFileVersion("3.4.2")]
+[assembly: AssemblyFileVersion("3.4.3")]
