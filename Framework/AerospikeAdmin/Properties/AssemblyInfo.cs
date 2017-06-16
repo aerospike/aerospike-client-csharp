7418afd1af5df0d930c46c3e9bc89129d02150bd
Brian Nichols <bnichols@aerospike.com>
2017-06-16 11:04:06 -0700

    Update version 3.4.4

--- Framework/AerospikeAdmin/Properties/AssemblyInfo.cs
+++ Framework/AerospikeAdmin/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.4")]
-[assembly: AssemblyFileVersion("3.4.3")]
+[assembly: AssemblyFileVersion("3.4.4")]
