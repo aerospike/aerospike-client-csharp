9fc622bd012f55c208a4d3f1bcb9394add1b7405
Brian Nichols <bnichols@aerospike.com>
2022-04-20 17:44:14 -0700

    Update version 4.2.7.1

--- Framework/AerospikeClient/Properties/AssemblyInfo.cs
+++ Framework/AerospikeClient/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.2.7")]
-[assembly: AssemblyFileVersion("4.2.7")]
+[assembly: AssemblyVersion("4.2.7.1")]
+[assembly: AssemblyFileVersion("4.2.7.1")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AerospikeTest")]
