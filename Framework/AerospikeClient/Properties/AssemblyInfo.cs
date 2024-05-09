85e4fb1945528cc58b81f394e76158d818f47f00
Shannon Klaus <sklaus@aerospike.com>
2024-05-09 10:48:18 -0600

    Update version 5.3.3

--- Framework/AerospikeClient/Properties/AssemblyInfo.cs
+++ Framework/AerospikeClient/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aerospike")]
 [assembly: AssemblyProduct("AerospikeClient")]
-[assembly: AssemblyCopyright("Copyright © 2012-2019")]
+[assembly: AssemblyCopyright("Copyright © 2012-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.3.2")]
-[assembly: AssemblyFileVersion("5.3.2")]
+[assembly: AssemblyVersion("5.3.3")]
+[assembly: AssemblyFileVersion("5.3.3")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AerospikeTest")]
