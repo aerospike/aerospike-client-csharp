92045f2dcbe6251a58861a3eaaa23058aa2dd1c4
Shannon Klaus <sklaus@aerospike.com>
2025-05-22 14:44:07 -0600

    Fix spacing

--- AerospikeClient/Policy/ClientPolicy.cs
+++ AerospikeClient/Policy/ClientPolicy.cs
@@ -268,19 +268,19 @@ namespace Aerospike.Client
 		/// <summary>
 		/// Flag to signify if alternate IP address discovery info commands should be used.
 		/// 
-	    /// If false, use:
-	    /// IP address: service-clear-std
-	    /// TLS IP address: service-tls-std
-	    /// Peers addresses: peers-clear-std
-	    /// Peers TLS addresses: peers-tls-std
-	    /// 
-	    /// If true, use:
-	    /// IP address: service-clear-alt
-	    /// TLS IP address: service-tls-alt
-	    /// Peers addresses: peers-clear-alt
-	    /// Peers TLS addresses: peers-tls-alt
-	    /// 
-	    /// Default: false
+		/// If false, use:
+		/// IP address: service-clear-std
+		/// TLS IP address: service-tls-std
+		/// Peers addresses: peers-clear-std
+		/// Peers TLS addresses: peers-tls-std
+		/// 
+		/// If true, use:
+		/// IP address: service-clear-alt
+		/// TLS IP address: service-tls-alt
+		/// Peers addresses: peers-clear-alt
+		/// Peers TLS addresses: peers-tls-alt
+		/// 
+		/// Default: false
 		/// </summary>
 		public bool useServicesAlternate;
 
