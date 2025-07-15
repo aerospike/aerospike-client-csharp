ef0fa1ba5994272335dfe5653d1fc9a0c217b3b7
Shannon Klaus <sklaus@aerospike.com>
2025-07-15 09:43:35 -0600

    Framework AerospikeTest signing as well

--- Framework/AerospikeClient/Properties/AssemblyInfo.cs
+++ Framework/AerospikeClient/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -35,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("5.3.3.1")]
 [assembly: AssemblyFileVersion("5.3.3.1")]
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AerospikeTest")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("AerospikeTest,PublicKey=0024000004800000940000000602000000240000525341310004000001000100a1d89dd60cb717\r\n5faea341a3099ac4244b82df753e994979fb4d88fb81e3885441db11c0651500ad5865d3502ee3\r\ndd3a0380d95b80b08a6e00beffd7bc04c1d50dbc6476bccae94251a6fc8800689dfb5d3642f5f4\r\nd698222395a33e47b19d78d3bbe5a8126c21049547a440b4d1c9bcf07bf8270267feadf2106aa1\r\n2f6b83b6")]
