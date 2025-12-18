25d0083089d9ca41b521971c7ceea067abe2ad1e
Shannon Klaus <sklaus@aerospike.com>
2025-12-18 08:23:17 -0700

    Try python client apporach to configuring server

--- .github/workflows/docker-setup/wait-for-as-server-to-start.bash
+++ .github/workflows/docker-setup/wait-for-as-server-to-start.bash
@@ -1,35 +1,30 @@
 #!/bin/bash
 
-set -x
-# Makes sure that if the "docker exec" command fails, it is not ignored
+# Makes sure that if the "docker run" command fails, it is not ignored
 set -o pipefail
 
-container_name=$1
-is_security_enabled=$2
+# We use bash because we need the not (!) operator
 
-if [[ $is_security_enabled == true ]]; then
-    # We need to pass credentials to asinfo if server requires it
-    # TODO: passing in credentials via command line flags since I can't figure out how to use --instance with global astools.conf
-    user_credentials="--user=admin --password=admin"
-fi
+call_from_tools_container() {
+    # See comment in run-ee-server.bash's call_from_tools_container() for why we surround $@ with double quotes
+    docker run --rm --network host aerospike/aerospike-tools "$@"
+}
 
 while true; do
-    # An unset variable will have a default empty value
-    # Intermediate step is to print docker exec command's output in case it fails
+    # Intermediate step is to send docker run command's output to stdout in case it fails
     # Sometimes, errors only appear in stdout and not stderr, like if asinfo throws an error because of no credentials
     # (This is a bug in asinfo since all error messages should be sent to stderr)
     # But piping and passing stdin to grep will hide the first command's stdout.
     # grep doesn't have a way to print all lines passed as input.
     # ack does have an option but it doesn't come installed by default
-    # shellcheck disable=SC2086 # The flags in user credentials should be separate anyways. Not one string
     echo "Checking if we can reach the server via the service port..."
-    if docker exec "$container_name" asinfo $user_credentials -v status | tee >(cat) | grep -qE "^ok"; then
+    # shellcheck disable=SC2086
+    if call_from_tools_container asinfo $SECURITY_FLAGS -v status | tee >(cat) | grep -qE "^ok"
+    then
         # Server is ready when asinfo returns ok
         echo "Can reach server now."
-        # docker container inspect "$container_name"
         break
     fi
-
     echo "Server didn't return ok via the service port. Polling again..."
 done
 
@@ -38,10 +33,15 @@ done
 while true; do
     echo "Waiting for server to stabilize (i.e return a cluster key)..."
     # We assume that when an ERROR is returned, the cluster is not stable yet (i.e not fully initialized)
-    if docker exec "$container_name" asinfo $user_credentials -v cluster-stable 2>&1 | (! grep -qE "^ERROR"); then
+    # When we use strong consistency, the server assumes all of its partitions are dead because it's running on a new
+    # storage device. That is why we ignore-migrations here
+    # shellcheck disable=SC2086
+    if call_from_tools_container asinfo $SECURITY_FLAGS -v "cluster-stable:ignore-migrations=true" 2>&1 | (! grep -qE "^ERROR")
+    then
         echo "Server is in a stable state."
         break
     fi
 
     echo "Server did not return a cluster key. Polling again..."
 done
+
