92cf01f7e5b0fb32f7f26d401c99664970f0aff1
Shannon Klaus <sklaus@aerospike.com>
2026-06-26 11:53:46 -0600

    Fix for batch writes do not set per-record inDoubt on timeout in split-retry path

--- AerospikeClient/Command/Batch.cs
+++ AerospikeClient/Command/Batch.cs
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2012-2025 Aerospike, Inc.
+ * Copyright 2012-2026 Aerospike, Inc.
  *
  * Portions may be licensed to Aerospike, Inc. under one or more contributor
  * license agreements.
@@ -808,13 +808,13 @@ namespace Aerospike.Client
 				{
 					if (ae.InDoubt)
 					{
-						SetInDoubt();
+						command.SetInDoubt();
 					}
 					status.SetException(ae);
 				}
 				catch (Exception e)
 				{
-					SetInDoubt();
+					command.SetInDoubt();
 					status.SetException(new AerospikeException(e));
 				}
 			}
